65f58a33338daef8895be75a9f0b6047da35de4d
Deborah <Deborah.Frija@ehl.ch>
2017-08-10 16:09:34 +0300

    Push

--- store.py
+++ store.py
@@ -1,11 +1,141 @@
 from bottle import route, run, template, static_file, get, post, delete, request
-from sys import argv
 import json
 import pymysql
+
+
+# Connect to the database
+connection = pymysql.connect(host='sql11.freesqldatabase.com',
+                             user='sql11189248',
+                             password='RAJh95pZfe',
+                             db='sql11189248',
+                             charset='utf8mb4',
+                             autocommit=True,
+                             cursorclass=pymysql.cursors.DictCursor)
+
 
 @get("/admin")
 def admin_portal():
 	return template("pages/admin.html")
+
+#LIST CATEGORIES
+@get("/categories")
+def list_categories():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM category ORDER BY name ASC;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return json.dumps({'STATUS':'SUCCESS', 'CATEGORIES':result, 'CODE':200})
+    except Exception:
+            return json.dumps({'STATUS':'ERROR', 'MSG':'Internal error', 'CODE':500})
+
+#LIST PRODUCTS
+@get("/products")
+def list_products():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM product ORDER BY title ASC;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return json.dumps({'STATUS':'SUCCESS', 'PRODUCTS':result, 'CODE':200})
+    except Exception:
+            return json.dumps({'STATUS':'ERROR', 'MSG':'Internal error', 'CODE':500})
+
+#LIST PRODUCTS BY CATEGORY
+@get("/category/<id>/products")
+def list_products_by_category(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM product WHERE category={} ;".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return json.dumps({'STATUS': 'SUCCESS', 'PRODUCTS':result, 'CODE':200})
+    except Exception:
+            return json.dumps({'STATUS':'ERROR', 'MSG':'Internal error', 'CODE':500})
+
+#GETTING PRODUCT
+@get("/product/<id>")
+def getting_product(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM product WHERE id={};".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return json.dumps({'STATUS':'SUCCESS', 'PRODUCTS':result, 'CODE':200})
+    except Exception:
+            return json.dumps({'STATUS':'ERROR', 'MSG':'Internal error', 'CODE':500})
+
+#DELETING PRODUCT
+@delete("/product/<id>")
+def deleting_product(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "DELETE FROM product WHERE id={0};".format(id)
+            cursor.execute(sql)
+            connection.commit()
+            return json.dumps({'STATUS':'SUCCESS', 'CODE':201})
+    except Exception:
+            return json.dumps({'STATUS':'ERROR', 'MSG':'Internal error', 'CODE':500})
+
+#DELETING CATEGORY
+@delete("/category/<id>")
+def deleting_category(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "DELETE FROM category WHERE id={0};".format(id)
+            sql1 = "DELETE FROM product WHERE category={0};".format(id)
+            cursor.execute(sql)
+            cursor.execute(sql1)
+            connection.commit()
+            return json.dumps({'STATUS':'SUCCESS', 'CODE':201})
+    except Exception:
+            return json.dumps({'STATUS':'ERROR', 'MSG':'Internal error', 'CODE':500})
+
+#CREATING CATEGORY
+@post('/category')
+def creating_categories():
+    try:
+        with connection.cursor() as cursor:
+            name = request.POST.get("name")
+            sql = "INSERT INTO category VALUES (id, '{}')".format(name)
+            cursor.execute(sql)
+            connection.commit()
+            return json.dumps ({'STATUS':'SUCCESS','CODE':201})
+    except Exception:
+        return json.dumps({'STATUS':'ERROR', 'MSG': 'Internal error', 'CODE': 500})
+
+#CREATING/EDIT PRODUCT
+@post('/product')
+def creating_products():
+    title = request.POST.get('title')
+    description = request.POST.get('desc')
+    price = request.POST.get('price')
+    img_url = request.POST.get('img_url')
+    category = request.POST.get('category')
+    favorite = request.POST.get('favorite')
+    if favorite:
+        fav = True
+    else:
+        fav= False
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT COUNT(*) FROM product WHERE title='{}'".format(title)
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            if result['COUNT(*)']>0:
+                sql_update = "UPDATE product SET description='{0}', price={1}, img_url='{2}', category={3}, favorite={4} WHERE title='{5}'".format(description, price, img_url, category, fav, title)
+                print sql_update
+                cursor.execute(sql_update)
+                product_id=0
+                connection.commit()
+            else:
+                sql_add = "INSERT INTO product VALUES (id, '{0}', '{1}', {2}, '{3}', {4}, {5})".format(title, description, price, img_url, category, fav)
+                cursor.execute(sql_add)
+                product_id = cursor.lastrowid
+                connection.commit()
+        return json.dumps ({'STATUS':'SUCCESS','PRODUCT_ID':product_id, 'CODE':201})
+    except Exception:
+        return json.dumps({'STATUS':'ERROR', 'MSG': 'Internal error', 'CODE': 500})
 
 
 
@@ -24,9 +154,12 @@ def stylesheets(filename):
     return static_file(filename, root='css')
 
 
-@get('/images/<filename:re:.*\.(jpg|png|gif|ico)>')
+@get('/images/<filename:re:.*\.(jpg|jpeg|png|gif|ico)>')
 def images(filename):
     return static_file(filename, root='images')
 
 
-run(host='0.0.0.0', port=argv[1])
+run(host='localhost', port=7000)
+
+if __name__ == '__main__':
+    main()
---
+++ store.sql
@@ -0,0 +1,100 @@
+CREATE TABLE category (
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(50));
+
+INSERT INTO category (name) VALUES ("France");
+INSERT INTO category (name) VALUES ("Switzerland");
+INSERT INTO category (name) VALUES ("Spain");
+
+CREATE TABLE product (
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(100),
+    description TEXT,
+    price FLOAT,
+    img_url VARCHAR(100),
+    category INTEGER,
+    favorite BOOLEAN);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES ( 
+		"Le 1947 ***", 
+		"Le 1947 derives its name from the most prestigious vintage of Château Cheval Blanc and provides the perfect stage for the striking culinary art of chef Yannick Alléno.",
+		"350.00",
+        "/images/le_1947.jpg",
+        6,
+        TRUE);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES (
+		"Flocons de Sel ***", 
+		"An intimate atmosphere, elegance down to the finest detail, the art of authentic mountain life honed to perfection: our gastronomic restaurant located at the heights of Megève offers diners a unique setting.",
+		"300.00",
+        "/images/flocons_de_sel.jpg",
+        6,
+        FALSE);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES (
+		"Plaza Athénée ***", 
+		"Alain Ducasse tells a personal and radical story, continuing the work he began twenty-five years ago. Healthier and more natural, more respectful of the Planet, it delivers a free and nearly instinctive interpretation of Haute Cuisine, revealing the produces’ original flavour, from the noble to the humble, all exceptional.",
+		"300.00",
+        "/images/plaza_athenee.jpg",
+        6,
+        FALSE);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES (
+		"Restaurant de l'Hôtel de Ville ***", 
+		"An exceptional setting to savor the moment. The Restaurant de l'Hotel de Ville de Crissier in Switzerland features Chef Franck Giovannini's signature cuisine.",
+		"400.00",
+        "/images/hotel_de_ville.jpg",
+        2,
+        TRUE);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES (
+		"Schloss Schauenstein ***", 
+		"Schauenstein Schloss Restaurant Hotel – a historical castle which features Andreas Caminada’s award-winning restaurant. Everybody does their best to provide an unforgettable experience.",
+		"350.00",
+        "/images/schloss.jpg",
+        2,
+        FALSE);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES (
+		"Cheval Blanc ***", 
+		"Our Chef de Cuisine Peter Knogl awaits you in the Cheval Blanc with a symphony of aromas, colours and harmonious tastes. Ingeniously prepared from the best nature has to offer us.",
+		"350.00",
+        "/images/cheval_blanc.jpg",
+        2,
+        FALSE);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES (
+		"El Celler de Can Roca ***", 
+		"El Celler de Can Roca is a restaurant in Girona, Catalonia, Spain which was opened in 1986 by the Roca brothers, Joan, Josep and Jordi.",
+		"300.00",
+        "/images/el_celler.jpg",
+        3,
+        TRUE);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES (
+		"Lasarte ***", 
+		"Lasarte is much more than chef Martín Berasategui’s gastronomic vision for Barcelona. It is a spiritual project with the very best team of professionals at its forefront that strives to delight its diners with every aroma and flavour.",
+		"250.00",
+        "/images/lasarte.jpg",
+        3,
+        FALSE);
+
+INSERT INTO product (title, description, price, img_url, category, favorite)
+	VALUES (
+		"DiverXo ***", 
+		"With food served on ‘canvasses’, and sauces painted on to dishes, the phrase ‘works of art’ certainly comes to mind when describing Spanish fusion restaurant DiverXo. With young Madrid-born chef David Muñoz at the pass it’s no surprise that hearty Iberian traditions meet the fragrant flavours of China and Japan.",
+		"250.00",
+        "/images/diver_xo.jpg",
+        3,
+        FALSE);
+
+SELECT * FROM product;
+select * from category
